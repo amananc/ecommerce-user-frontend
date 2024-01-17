2a3a1afa80bb914ab47b50b14a459f3b4a8bc982
Aman Anchal <amananc06@gmail.com>
2024-01-18 01:30:24 +0530

    initial commit

---
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './NewsLetter.css'
+
+const NewsLetter = () => {
+  return (
+    <div className='newsLetter'>
+        <h1> Get Exclusive Offers on Your Email </h1>
+        <p> Subscribe to our newsletter and stay updated</p>
+
+        <div>
+            <imput type ="email" placeholder="Your Email Id" />
+            <button> Subscribe </button>
+        </div>
+    </div>
+  )
+}
+
+export default NewsLetter
